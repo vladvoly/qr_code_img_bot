33b4444e93b53a01087eb66118c58c772a2230ba
Vlad <50437664+vladvoly@users.noreply.github.com>
2022-06-25 10:43:48 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -8,9 +8,9 @@ TOKEN = os.environ['MY_BOT_TOKEN']
 bot = telebot.TeleBot(TOKEN)
 
 #Picture output function
-def out_qr(in_t,in_c,in_q,mci):
+def out_qr(in_t,in_q,mci):
 	markup = types.ReplyKeyboardRemove()
-	out_url = 'http://api.qrserver.com/v1/create-qr-code/?size=512x512&margin=5&data='+in_t+'&color='+in_c+'&ecc='+in_q
+	out_url = 'https://qrcode.tec-it.com/API/QRCode?data='+in_t+'&errorcorrection='+in_q+'&backcolor=%23ffffff&quietzone=5&size=Large'
 	markup = types.ReplyKeyboardRemove()
 	bot.send_photo(mci, out_url, reply_markup=markup, caption='Your QR-Code')
 
@@ -25,9 +25,8 @@ def send_welcome(message):
 	if message.text == '/help':
 		out_start = "OK. I will help you.\n"
 		out_start += "1) - Enter the text or URL - *Hello World! *\n"
-		out_start += "2) - Choose the degree of resistance of QR-Code to damages - *Low/Medium/Quality/High*\n"
-		out_start += "3) - Choose a QR-Code color - *Red/Green/Blue/Black/Other*\n"
-		out_start += "4) - Get your QR-Code\n"
+		out_start += "2) - Choose the degree of resistance of QR-Code to damages - *Low/Medium/High*\n"
+		out_start += "3) - Get your QR-Code\n"
 		bot.send_message(message.chat.id, out_start, reply_markup=markup, parse_mode="Markdown")
 
 #Commands to process the received data from the telegram
@@ -35,45 +34,13 @@ def send_welcome(message):
 def send_text(message):
 	
 	ms_quality = ('Low', 'Medium', 'Quality', 'High')
-	ms_color = ('Red', 'Green', 'Blue', 'Black')
 	global in_text
 	global in_quality
-	global in_color 
 
 	#2 - got the value of stability QR-code to damage
 	if message.text in ms_quality:
 		in_quality = message.text
 		in_quality = in_quality[0]	
-		markup = types.ReplyKeyboardMarkup(row_width=5, resize_keyboard=True)
-		itembtn1 = types.KeyboardButton('Red')
-		itembtn2 = types.KeyboardButton('Green')
-		itembtn3 = types.KeyboardButton('Blue')
-		itembtn4 = types.KeyboardButton('Black')
-		itembtn5 = types.KeyboardButton('Other')
-		markup.add(itembtn1, itembtn2, itembtn3,itembtn4,itembtn5)
-		bot.send_message(message.chat.id, "Enter color QR-Code", reply_markup=markup)
-	
-	#3 - got the color value of QR-Code
-	elif message.text in ms_color:
-	 	in_color = message.text
-	 	if in_color == 'Red':
-	 		in_color = 'FF0000'
-	 	elif in_color == 'Green':
-	 		in_color = '00FF00'
-	 	elif in_color == 'Blue':
-	 		in_color = '0000FF'
-	 	elif in_color == 'Black':
-	 		in_color = '000000'
-	 	out_qr(in_text,in_color,in_quality,message.chat.id)
-
-	#4 - custom color request
-	elif message.text == 'Other':
-		markup = types.ReplyKeyboardRemove()
-		bot.send_message(message.chat.id, "Enter your color in HTML HEX format", reply_markup=markup)
-
-	#5 - output QR Code with custom color
-	elif message.text[0] == '#':
-		in_color = message.text[1:]
 		out_qr(in_text,in_color,in_quality,message.chat.id)
 
 	#1 - came plain text...
